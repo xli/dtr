e3f3d8f6d554fd6bc731774fb31273228ff03480
Li Xiao <iam@li-xiao.com>
2008-09-02 20:12:16 +0800

    added growling_test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'logger'
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../testdata')
 
+require 'rubygems'
+require 'growling_test'
+
 ENV['DTR_ENV'] = 'test'
 
 module Test
