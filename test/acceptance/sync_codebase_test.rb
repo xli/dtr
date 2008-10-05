407c9b17eff0c8d09cab989d3f5b6be12ad932b5
Li Xiao <iam@li-xiao.com>
2008-10-05 14:13:50 +0800

    more reasonable test for: dont sync codebase and setup new working env for agent is in same dir with master process

--- test/acceptance/sync_codebase_test.rb
+++ test/acceptance/sync_codebase_test.rb
@@ -40,20 +40,13 @@ class SyncCodebaseTest < Test::Unit::TestCase
   end
 
   def test_should_not_sync_codebase_and_setup_working_dir_when_agent_is_in_same_dir_with_master_process
-    testdata_dir = File.expand_path(File.dirname(__FILE__) + '/../../testdata')
-    Dir.chdir(testdata_dir) do
-      require 'a_test_case'
-      require 'a_test_case2'
-      require 'a_file_system_test_case'
+    @master_dir = File.expand_path(File.dirname(__FILE__) + '/../../testdata/should_not_sync_codebase_and_setup_working_dir_when_agent_is_in_same_dir_with_master_process')
+    Dir.chdir(@master_dir) do
+      require 'verify_dir_pwd_test_case'
     end
-    $argv_dup = ['a_test_case.rb', 'a_test_case2.rb', 'a_file_system_test_case.rb']
-    suite = Test::Unit::TestSuite.new('run_test_passed')
-    suite << ATestCase.suite
-    suite << ATestCase2.suite
-    suite << AFileSystemTestCase.suite
-    @master_dir = "#{testdata_dir}_copy"
-    FileUtils.rm_rf(@master_dir)
-    FileUtils.cp_r testdata_dir, @master_dir
+    $argv_dup = ['verify_dir_pwd_test_case.rb']
+    suite = Test::Unit::TestSuite.new('test_should_not_sync_codebase_and_setup_working_dir')
+    suite << VerifyDirPwdTestCase.suite
     @agent = start_agent_at @master_dir, 2, false
     begin
       DTR.inject
@@ -61,15 +54,13 @@ class SyncCodebaseTest < Test::Unit::TestCase
         Dir.chdir(@master_dir) do
           result = runit(suite)
           assert result.passed?
-          assert_equal 3, result.run_count
+          assert_equal 1, result.run_count
         end
       end
-      assert !File.exists?(File.join(@master_dir, Socket.gethostname.gsub(/[^a-zA-Z0-9]/, '_')))
     ensure
       DTR.reject
       Process.kill 'TERM', @agent rescue nil
       Process.waitall
-      FileUtils.rm_rf(@master_dir)
     end
   end
 
