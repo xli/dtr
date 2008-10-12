6330c8663bf070858302868c7087813895b27df7
Li Xiao <iam@li-xiao.com>
2008-10-12 15:55:01 +0800

    added test for DTR::TestTask

---
+++ test/acceptance/raketasks_test.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/../test_helper'
+include DTR::AgentHelper
+
+class RakeTasksTest < Test::Unit::TestCase
+  def test_test_task
+    FileUtils.mkdir_p("raketasks_test_agent")
+    Dir.chdir('raketasks_test_agent') do
+      start_agents
+    end
+    test_dir = File.expand_path(File.dirname(__FILE__) + '/../../testdata/raketasks')
+    Dir.chdir(test_dir) do
+      output = %x[rake dtr]
+      expected = <<-OUTPUT
+1 tests, 1 assertions, 0 failures, 0 errors
+OUTPUT
+      assert_equal 0, $?.exitstatus
+      assert output.include?(expected), "should include #{expected}"
+    end
+  ensure
+    stop_agents
+    FileUtils.rm_rf("raketasks_test_agent")
+  end
+end
---
+++ testdata/raketasks/success_test_case.rb
@@ -0,0 +1,6 @@
+require 'test/unit'
+class SuccessTestCase < Test::Unit::TestCase
+  def test_succeeded
+    assert true
+  end
+end
