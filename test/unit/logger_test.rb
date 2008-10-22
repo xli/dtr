71f329e4525fefcdf63396a235e46d61f6f26082
Li Xiao <iam@li-xiao.com>
2008-10-22 22:51:59 +0800

    added test for output error msg into $stderr

--- test/unit/logger_test.rb
+++ test/unit/logger_test.rb
@@ -1,8 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 require 'dtr/agent/sync_logger'
-
-#SyncLoggerTest is acceptance
+require 'stringio'
+#logger related unit test, including sync_logger
 class LoggerTest < Test::Unit::TestCase
   def setup
     @msg = nil
@@ -31,6 +31,27 @@ class LoggerTest < Test::Unit::TestCase
     DTR.logger = DTR::SyncLogger::MessageDecoratedLogger.new(self)
     DTR.info { 'info msg' }
     assert_equal "From #{Socket.gethostname}: info msg", @msg
+  end
+
+  def test_should_output_error_into_stderr
+    logs = catch_stderr_output do
+      DTR.debug {'debug info'}
+      DTR.info {'info info'}
+      DTR.error {'error info'}
+    end
+    assert(/error info/ =~ logs)
+    assert(/info info/ !~ logs)
+    assert(/debug info/ !~ logs)
+  end
+
+  def catch_stderr_output
+    stderr = $stderr
+    $stderr = StringIO.new
+    yield
+    $stderr.rewind
+    $stderr.read
+  ensure
+    $stderr = stderr
   end
 
   def level
