2641e2d59de6a8367040fc662279f7d1526ef6b0
Li Xiao <iam@li-xiao.com>
2008-10-25 14:29:38 +0800

    added rails ext for initializing database

---
+++ lib/dtr/agent/rails_ext.rb
@@ -0,0 +1,46 @@
+# Copyright (c) 2007-2008 Li Xiao <iam@li-xiao.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module DTR
+  module Agent
+    module RailsExt
+      module DatabaseInitializer
+        def initialize_database
+          unless File.exist?('./config/database.yml.dtr')
+            DTR.info("No ./config/database.yml.dtr exists, bypass initializing database task.")
+            return
+          end
+          FileUtils.cp('./config/database.yml.dtr', './config/database.yml')
+          unless Cmd.execute('rake db:drop:all db:create:all db:migrate db:test:prepare --trace')
+            raise 'Initialize database failed!'
+          end
+        end
+      end
+
+      module WorkingEnvExt
+        include DatabaseInitializer
+        def self.included(base)
+          base.alias_method_chain :load_environment, :preparing_database
+        end
+
+        def load_environment_with_preparing_database(&block)
+          load_environment_without_preparing_database do
+            initialize_database if defined?(ActiveRecord::Base)
+            block.call
+          end
+        end
+      end
+    end
+  end
+end
