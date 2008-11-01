fe29892880e67ce53829b7314212ad1fdc2cf4f2
Li Xiao <iam@li-xiao.com>
2008-11-01 19:22:43 +0800

    added a test for raketask DTR::TestTask start test with agent

--- test/acceptance/raketasks_test.rb
+++ test/acceptance/raketasks_test.rb
@@ -20,4 +20,18 @@ OUTPUT
     stop_agents
     FileUtils.rm_rf("raketasks_test_agent")
   end
+
+  def test_test_task_with_processes
+    test_dir = File.expand_path(File.dirname(__FILE__) + '/../../testdata/raketasks')
+    Dir.chdir(test_dir) do
+      output = %x[rake dtr P=3]
+      expected = <<-OUTPUT
+1 tests, 1 assertions, 0 failures, 0 errors
+OUTPUT
+      assert_equal 0, $?.exitstatus
+      assert output.include?(expected), "should include #{expected}"
+    end
+  ensure
+    FileUtils.rm_rf("#{test_dir}/#{Socket.gethostname.gsub(/[^\d\w]/, '_')}")
+  end
 end
